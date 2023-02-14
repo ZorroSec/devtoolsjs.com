18e937e88b1c02c64dd70ac8302b06dc01915cbb
ZorroSec <joseiraildesciprianoribeiro@gmail.com>
2023-02-14 19:14:29 -0300

    add

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,14 +1,16 @@
 # from app.pages import Pages
 # from app import app
-# from flask import render_template
-# from flask import redirect
-# from flask import url_for
+from flask import render_template
+from flask import redirect
+from flask import url_for
 from colorama import Fore
+from json import loads, load, dump
 
 try:
     name = str(input(f"{Fore.LIGHTBLACK_EX}username > "))
     password = str(input(f"{Fore.LIGHTBLACK_EX}password > "))
     with open('data.json', 'w') as data:
         data = data.write('{ "username": "%s", "password": "%s" }'%(name, password))
+
 except:
     pass
