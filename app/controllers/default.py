a499278a7d38487d46e8c226129e186a000ba4bf
ZorroSec <joseiraildesciprianoribeiro@gmail.com>
2023-02-14 19:24:28 -0300

    upgrade in default.py

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -5,16 +5,6 @@ from flask import redirect
 from flask import url_for
 from colorama import Fore
 from json import loads, load, dump
-
-try:
-    name = str(input(f"{Fore.LIGHTBLACK_EX}username > "))
-    password = str(input(f"{Fore.LIGHTBLACK_EX}password > "))
-    with open('data.json', 'w') as data:
-        data = data.write('{ "username": "%s", "password": "%s" }'%(name, password))
-except KeyboardInterrupt as e:
-    print(f"{Fore.LIGHTRED_EX}Error {e}")
-
-
 config = Pages(name, password)
 
 
