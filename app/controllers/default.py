79a50cbf60dc483aa0775424156d3fa318c89af7
ZorroSec <joseiraildesciprianoribeiro@gmail.com>
2023-02-19 10:09:44 -0300

    <>

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -5,8 +5,11 @@ from flask import redirect
 from flask import url_for
 from colorama import Fore
 from json import loads, load, dump
-config = Pages(name, password)
 
+name = input(f"{Fore.LIGHTBLACK_EX} username > ")
+passw = input(f"{Fore.LIGHTBLACK_EX} password > ")
+with open("data.json", 'w') as file:
+    dump({ "username": f"{name}", "password": f"{passw}" }, file)
 
 @app.route('/')
 def home():
