93938ea97d32df2a7deeef12de89cc6d25dbaf31
ZorroSec <joseiraildesciprianoribeiro@gmail.com>
2023-02-19 11:45:07 -0300

    fatality

--- main.sh
+++ main.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
-python3 index.py
+
+GREEN="\e[1;92m"
+BLACK="\e[1;90m"
+
+echo -e "$BLACK
+/===========================\\
+/  [01] Run index.py         \\
+/  [02] Run index.js          \\
+/==============================\\
+"
+read op
+
+case $op in
+    1)python3 index.py
+    ;;
+    2)node index.js
+    ;;
+esac
